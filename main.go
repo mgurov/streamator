10e1d98b5e1c5aaec9824f20cf0af1bfb2f7c094
Mykola Gurov <ngurov@gmail.com>
2017-07-11 22:57:47 +0200

    proper sync via the quit chan and wait group to exit the process

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
-package main 
+package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
 )
 
 func main() {
-	ticker := time.Tick(1 * time.Second)
 
-	for _ = range ticker {
-		println("hello, world", time.Now().String())
-	}
+	quit := make(chan interface{})
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		for {
+			select {
+			case t := <-ticker.C:
+				println("Hello, world", time.Now().Sub(t).String())
+			case <-quit:
+				println("Received signal to stop")
+				ticker.Stop()
+				wg.Done()
+				return
+			}
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		for _ = range signalChan {
+			fmt.Println("\nReceived an interrupt, stopping services...")
+			quit <- nil
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+
 }
