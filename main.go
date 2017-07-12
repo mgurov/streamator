795abf7b6833475de80bef3fb8bf74fa0e45c897
Mykola Gurov <ngurov@gmail.com>
2017-07-12 23:47:03 +0200

    our hooker for logs

--- main.go
+++ main.go
@@ -6,12 +6,67 @@ import (
 	"os/signal"
 	"sync"
 	"time"
+
 	"github.com/sirupsen/logrus"
 )
 
-var log = logrus.New()
+var b *logrus.Hook
+
+type cappedInMemoryRecorderHook struct {
+	m sync.Mutex
+	records []*logrus.Entry
+	wIndex  int
+	owerwrites bool
+}
+
+func newCappedInMemoryRecorderHook(c int) *cappedInMemoryRecorderHook {
+	if c <= 0 {
+		panic("cappedInMemoryRecorderHook should be not empty size of")
+	}
+	return &cappedInMemoryRecorderHook{
+		records: make([]*logrus.Entry, c),
+	}
+}
+
+func (h *cappedInMemoryRecorderHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (h *cappedInMemoryRecorderHook) Fire(e *logrus.Entry) error {
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	h.records[h.wIndex] = e
+
+	h.wIndex ++
+	if h.wIndex >= len(h.records) {
+		h.wIndex = 0
+		h.owerwrites = true
+	}
+	return nil
+}
+
+func (h *cappedInMemoryRecorderHook) Copy() []*logrus.Entry {
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	if (!h.owerwrites) {
+		//todo: test me now
+		return h.records[:h.wIndex]
+	} 
+
+	result := make([]*logrus.Entry, len(h.records))
+
+	copy(result, h.records[h.wIndex:])	
+	copy(result[h.wIndex:], h.records[:h.wIndex])	
+	return result
+}
 
 func main() {
+
+	ourHook := newCappedInMemoryRecorderHook(20)
+	var log = logrus.New()
+	log.Hooks.Add(ourHook)
 
 	quit := make(chan interface{})
 	var wg sync.WaitGroup
@@ -24,7 +79,7 @@ func main() {
 			case t := <-ticker.C:
 				log.WithField("type", "repeating").
 					WithField("duration", time.Now().Sub(t)).
-					Error("Hello, world ", )
+					Error("Hello, world ")
 			case <-quit:
 				ticker.Stop()
 				wg.Done()
@@ -45,4 +100,6 @@ func main() {
 	}()
 
 	wg.Wait()
+
+	fmt.Printf("%#v\n", ourHook.Copy())
 }
